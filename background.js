4139c0dc9689061ee2298bfe6187e6bbd994acdc
Yuxin Zhou <yuxin.zhou2@mail.mcgill.ca>
2019-10-12 15:28:06 -0400

    prettier

--- background.js
+++ background.js
@@ -1,20 +1,21 @@
 function startTime() {
-    var start = moment();
-    var timer = setInterval(function() {
-        var diff = moment().diff(start, 'seconds');
-        updateTime(diff);
-    });
+  var start = moment();
+  var timer = setInterval(function() {
+    var diff = moment().diff(start, "seconds");
+    updateTime(diff);
+  });
 }
 
 function notifyUser() {
-    var opts = {
-        "type":"basic",
-        "title":"",
-        "message":""
-    };
-    var idBase = "";
-    var id = idBase +
-    chrome.notifications.create(id, opts, function(){
-        console.log(idBase+ "created");
+  var opts = {
+    type: "basic",
+    title: "",
+    message: ""
+  };
+  var idBase = "";
+  var id =
+    idBase +
+    chrome.notifications.create(id, opts, function() {
+      console.log(idBase + "created");
     });
 }
