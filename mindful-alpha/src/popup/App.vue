cf0261cab709d0d5643ea75b8737beff33f7248e
Yuxin Zhou <yuxin.zhou2@mail.mcgill.ca>
2019-11-11 16:30:52 -0500

    jquery

--- mindful-alpha/src/popup/App.vue
+++ mindful-alpha/src/popup/App.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
-    <p>Hello world!</p>
+    <h1>Mindful</h1>
+    <div class="coding-settings">
+      <h2>Coding Questions Settings</h2>
+      <div class="programming-language">
+        <label for="programming-language-picker">Programming Language</label>
+        <select id="programming-language-picker">
+          <option value="java">Java</option>
+          <option value="c">C</option>
+          <option value="python">Python</option>
+        </select>
+      </div>
+      <div class="difficulty">
+        <label for="difficulty-picker">Difficulty</label>
+        <select id="difficulty-picker">
+          <option value="easy">Easy</option>
+          <option value="intermediate">Intermediate</option>
+          <option value="hard">Hard</option>
+        </select>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {}
+  data() {
+    return {};
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
